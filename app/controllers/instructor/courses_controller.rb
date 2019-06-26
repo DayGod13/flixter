6e5d1657092f9970f2d36ec50e55601c4ff06a34
Tobias Pasquale <tobiaspasquale@gmail.com>
2019-06-26 04:28:29 +0000

    Added a student view of the lessons detail page.

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -27,7 +27,6 @@ class Instructor::CoursesController < ApplicationController
         end
     end
 
-
     helper_method :current_course
     def current_course
         @current_course ||= Course.find(params[:id])
