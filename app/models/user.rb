aea5731a5535af01b9ecb6c4d8034c2ba44ecf6b
Tobias Pasquale <tobiaspasquale@gmail.com>
2019-07-04 05:33:33 +0000

    Added student enrollment DB and functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_many :courses
+  has_many :enrollments
+  has_many :enrolled_courses, through: :enrollments, source: :course
 
   
   # Include default devise modules. Others available are:
@@ -7,5 +9,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+
+  def enrolled_in?(course)
+    return enrolled_courses.include?(course)
+  end
   
 end
